86fa395c6c0ef686dca0939cbd1ffc277b0eb0cc
Mateusz Pietrzak <mateusz@mzpk.dev>
2025-04-18 14:39:43 +0200

    include playwright's `chromium.connectOverCDP` test

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
+import { spawn } from "child_process"
 import puppeteer from "puppeteer"
 import { chromium } from "playwright"
 
 
 async function main() {
     const EXECUTABLE_PATH = "/Applications/Figma.app/Contents/MacOS/Figma"
-    // Try replacing `use_playwright` with `use_puppeteer`, puppeteer seems not to have this issue
-    // await use_puppeteer(EXECUTABLE_PATH)
-    await use_playwright(EXECUTABLE_PATH)
+    // Try replacing `use_playwright_launch` with `use_puppeteer_launch`, puppeteer seems not to have this issue
+    // await use_puppeteer_launch(EXECUTABLE_PATH)
+    // await use_playwright_launch(EXECUTABLE_PATH)
+    await use_playwright_connectOverCDP(EXECUTABLE_PATH)
 }
 
-const use_puppeteer = async (executablePath) => {
+const use_puppeteer_launch = async (executablePath) => {
     await puppeteer.launch({
         executablePath,
         args: ["--remote-debugging-port=9222"]
@@ -17,7 +19,7 @@ const use_puppeteer = async (executablePath) => {
     console.log("This code is always being reached")
 }
 
-const use_playwright = async (executablePath) => {
+const use_playwright_launch = async (executablePath) => {
     await chromium.launch({
         executablePath,
         // Try commenting out the line below, this seems to be causing the issue
@@ -25,6 +27,19 @@ const use_playwright = async (executablePath) => {
     })
     console.log("This code is never being reached")
 }
+
+const use_playwright_connectOverCDP = async (executablePath) => {
+    await spawn(executablePath, ["--remote-debugging-port=9222"])
+    await sleep(3000)
+    const status = await fetch("http://localhost:9222/json/version")
+        .then(response => response.json())
+    console.log("Port 9222 status: ", status)
+    await chromium.connectOverCDP("http://localhost:9222")
+    console.log("This code is never being reached")
+}
+
+const sleep = time =>
+    new Promise(resolve => setTimeout(resolve, time));
 
 
 main()
